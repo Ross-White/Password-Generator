3791781df1a2d66becfb93d508e70fae42b04352
Ross White <rosswhite@outlook.com>
2021-03-02 18:47:38 +0000

    Adding prompts for inputs

--- Main/script.js
+++ Main/script.js
@@ -10,5 +10,13 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like? It must be between 8 and 128 characters.");
+  var lowerCase = confirm("Do you want lowercase in your password?");
+  var upperCase = confirm("Do you want upper case in your password?");
+  var special = confirm("Do you want special characters in you password?");
+  var minimumCount = 0;
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
