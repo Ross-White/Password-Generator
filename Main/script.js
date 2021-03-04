e7b3da2d35a7b6b05d015eaa3ba6e3ee868cffcc
Ross White <rosswhite@outlook.com>
2021-03-04 17:59:05 +0000

    Adding passwordLength verifier

--- Main/script.js
+++ Main/script.js
@@ -1,22 +1,34 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var special = "!£$%&*@?~{}()[]";
+var number = "0123456789";
+var characters = "";
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
-}
-
-function generatePassword() {
-  var passwordLength = prompt("How many characters would you like? It must be between 8 and 128 characters.");
-  var lowerCase = confirm("Do you want lowercase in your password?");
-  var upperCase = confirm("Do you want upper case in your password?");
-  var special = confirm("Do you want special characters in you password?");
-  var minimumCount = 0;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var passwordLength = prompt("How many characters would you like? It must be between 8 and 128 characters.");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Password must be 8 - 128 characters");
+    return generatePassword()
+  }
+  
+  var getLowerCase = confirm("Do you want lowercase in your password?");
+  var getUpperCase = confirm("Do you want uppercase in your password?");
+  var getSpecial = confirm("Do you want special characters in you password?");
+  var getNumber = confirm("Do you want numbers in your password?");
+
+  
+}
+
